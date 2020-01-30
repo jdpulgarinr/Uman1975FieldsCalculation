226036a9b7be1ee9257624695acffcdc7061a1a2
Juan Diego <jdpulgarinr@gmail.com>
2020-01-30 14:02:01 -0500

    Made it a function

--- EField.m
+++ EField.m
@@ -1,74 +1,94 @@
+function [timeVector, EFieldVector] = EField(calculationDistance)
+%EField This function calculates the electric field at a distant point from
+%a lightning channel by means of numerical integration of Equation (9) of
+%the paper: The Electromagnetic Radiation from a Finite Antenna, by Martin
+%A. Uman, D. Kenneth McLain, E. Philip Krider. 1975.
+%
+% Syntax:  [timeVector, EFieldVector] = EField(calculationDistance)
+%
+% Inputs:
+%    calculationDistance:   This is the horizontal distance from the
+%    lightning channel to the observation point of the field.
+%
+% Outputs:
+%    timeVector:     The time vector used for the calculation of the field.
+%
+%    EFieldVector:   The electric field vector calculated at each element of
+%    the time vector.
+%
+% Other m-files required: piecewiseCurrent.m
+% Subfunctions: none
+% MAT-files required: none
 
+% Author: Juan Pulgarin
+% email: jdpulgarinr@gmail.com
+% June, 2014.
 
-%% Constantes
-c = 299792458;
-v = 8e7;
-u0 = 4*pi*(1e-7);
-e0 = 8.8541878176*(1e-12);
-H = 4000;
+%------------- BEGIN CODE --------------
 
-D = 1e3;
+%% CONSTANTS
+    % Empty Space
+    u0 = 4*pi*1e-7;
+    e0 = 8.8541878176*1e-12;
+    c = 1/sqrt(u0*e0);
 
-% Tiempo de simulaci?n y particiones del tiempo
-t = linspace(0,90e-6,1000);
+    % Speed of the Return-Stroke Front
+    v=8e7;
 
-% Matriz de elementos para almacenar los valores del campo el?ctrico
-E = zeros(1,length(t));
+    % Channel Height
+    H = 4000;
 
-% Particiones para integraci?n con respecto a z y vector de pesos para
-% integral simple
-n = 10000;
-w = zeros(1,(n+1)) + 2;
-w(1,1) = 1;
-w(1,(n+1)) = 1;
+    % Observation Distance from Channel
+    D = calculationDistance;
 
-% Particiones para integraci?n con respecto al tiempo y vectores de pesos
-% para integral doble
-m = 1000;
-Q = w;
-P = zeros((m+1),1) + 2;
-P(1,1) = 1;
-P((m+1),1) = 1;
+%% VECTORS INITIALIZATION
+    % Time Vector
+    timeVector=linspace(0,90e-6,1000);
 
-F = zeros((m+1),(n+1));
+    % Electric Field Array Initialization
+    E = zeros(1,length(timeVector));
+    
+%% INTEGRATION
+    % Channel Partitions and Weights Vector for Single Integration
+    n = 10000;
+    w = zeros(1,(n+1)) + 2;
+    w(1,1) = 1;
+    w(1,(n+1)) = 1;
 
-%% Integraci?n
+    % Time Partitions and Wights Vector for Double Integration
+    m = 1000;
+    Q = w;
+    P = zeros((m+1),1) + 2;
+    P(1,1) = 1;
+    P((m+1),1) = 1;
 
-    % T?rminos independientes del tiempo
+    % Function Evaluations Array Initialization
+    F = zeros((m+1),(n+1));
+
+    % Time-Independent Terms
     Z = 0:(H/n):H;
     R = sqrt((D^2) + (Z.^2));
     TH = (atan(Z./D)) + (pi/2);
     
-    % T?rminos independientes del tiempo en la integral de campo el?ctrico
+    % Time-Independent Terms in the Electric Field Integral
     T3 = ((2 - (3*((sin(TH)).^2)))./(R.^3)); 
     T4 = ((2 - (3*((sin(TH)).^2)))./(c*(R.^2)));
     T5 = (((sin(TH)).^2)./((c^2)*R));
 
-for k = 1:length(t)
-    % Campo el?ctrico
-    [I,DI] = piecewiseCurrent(Z,R,t(k),v);
+    % Time-Marching Loop for Integration
+    for k = 1:length(timeVector)
+        [I,DI] = piecewiseCurrent(Z,R,timeVector(k),v);
 
-    TAU = 0:(t(k)/m):t(k);
+        TAU = 0:(timeVector(k)/m):timeVector(k);
 
-    for d = 1:length(TAU)
-        [Itau,Dtau] = piecewiseCurrent(Z,R,TAU(d),v);
-        F(d,:) = T3.*Itau;
+        for d = 1:length(TAU)
+            [Itau,~] = piecewiseCurrent(Z,R,TAU(d),v);
+            F(d,:) = T3.*Itau;
+        end
+
+        BB4 = T4.*(I);
+        BB5 = T5.*(DI);
+        E(1,k) = (1/(2*pi*e0))*((((H*timeVector(k))/(4*m*n))*(Q*F'*P)) + (((H/(2*n))*(BB4*w'))) - (((H/(2*n))*(BB5*w'))));
     end
-
-    BB4 = T4.*(I);
-    BB5 = T5.*(DI);
-    E(1,k) = (1/(2*pi*e0))*((((H*t(k))/(4*m*n))*(Q*F'*P)) + (((H/(2*n))*(BB4*w'))) - (((H/(2*n))*(BB5*w'))));
+    EFieldVector = E;
 end
-
-%% Gr?fica del campo el?ctrico
-figure
-set(gcf,'units','normalized','outerposition',[0 0 1 1]);
-plot(t,E,'LineWidth',2,'Color','k'), grid on, hold on;
-%plot(t,(E(2,:)*10),'LineWidth',2,'Color','b');
-%plot(t,(E(3,:)*100),'LineWidth',2,'Color','r');
-%xlabel('Time [s]','Interpreter','LaTeX','FontSize',30), ylabel('$||\vec{E}|| \times 10^{-2}$ Field [V/m]','Interpreter','LaTex','FontSize',30)
-title('Electric Field','Interpreter','LaTeX','FontSize',30)
-%set(gca,'FontName','Times New Roman','FontSize',30)
-%axis([0 tf 1.2*min([min(E(1,:)) min(E(2,:)*10) min(E(3,:)*100)]) 1.2*max([max(E(1,:)) max(E(2,:)*10) max(E(3,:)*100)])])
-%lg = legend('$D = 1$ $km$ ($\times 1$)','$D = 10$ $km$ ($\times 10$)','$D = 100$ $km$ ($\times 100$)');
-%set(lg,'Interpreter','LaTeX')
